c162b734d76e5ca6e957a550d5110a8151827fb4
Breno Vargas <breno_fari@hotmail.com>
2023-12-14 10:13:57 -0300

    matrix transpose

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include <chrono>
 #include <iostream>
 
+#define MATRIX_TRANSPOSE_TEST
+
 int main() {
+
+#ifdef MATRIX_MULTIPLY_TEST
   const int matrix_size = 200;
   Matrix *a = new Matrix(matrix_size, matrix_size);
   Matrix *b = new Matrix(matrix_size, matrix_size);
@@ -27,6 +31,39 @@ int main() {
   duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time2 -
                                                                   start_time2);
   std::cout << "\n parallel " << duration.count() / 1000000.f << ".ms\n";
+
+  std::cout << '\n';
+  result->print();
+
+#endif // MATRIX_MULTIPLY_TEST
+
+#ifdef MATRIX_TRANSPOSE_TEST
+  const int columns = 2000;
+  const int rows = 4500;
+  Matrix *a = new Matrix(rows, columns);
+  Matrix *result = new Matrix(columns, rows);
+
+  a->set_all(1);
+  a->set_value(1, 1, 2);
+  a->set_value(2, 2, 2);
+  a->set_value(3, 3, 2);
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+  Matrix::transpose(a, result);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      end_time - start_time);
+  std::cout << "sequential transpose: " << duration.count() / 1000000.f
+            << ".ms\n";
+
+  auto start_time2 = std::chrono::high_resolution_clock::now();
+  Matrix::parallel_transpose(a, result);
+  auto end_time2 = std::chrono::high_resolution_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time2 -
+                                                                  start_time2);
+  std::cout << "\n parallel " << duration.count() / 1000000.f << ".ms\n";
+
+#endif // MATRIX_TRANSPOSE_TEST
 
   return 0;
 }
--- matrix.h
+++ matrix.h
@@ -27,7 +27,7 @@ public:
     for (size_t i = 0; i < length; i++) {
       std::cout << data[i] << " ";
 
-      if ((i + 1) % rows == 0) {
+      if ((i + 1) % columns == 0) {
         std::cout << "\n";
       }
     }
@@ -50,6 +50,11 @@ public:
   }
 
   static void parallel_multiply(Matrix *x, Matrix *y, Matrix *result) {
+    if (!(x->columns == y->rows) ||
+        !((x->rows == result->rows) && (y->columns == result->columns))) {
+      std::cout << "Error: unable to parallel multiply incompatible Matrixes\n";
+    }
+
     struct process_data_chunk {
       void operator()(Matrix *x, Matrix *y, Matrix *result, int start_index,
                       int end_index) {
@@ -92,6 +97,89 @@ public:
       }
 
       process_data_chunk()(result, x, y, block_end, result_length);
+    }
+  }
+
+  static void transpose(Matrix *x, Matrix *result) {
+    if (!((x->columns == result->rows) && x->rows == result->columns)) {
+      std::cout << "Error: invalid matrix sizes for transpose.\n";
+    }
+
+    int result_length = result->rows * result->columns;
+
+    int result_row = 0;
+    int result_column = 0;
+
+    int input_row = 0;
+    int input_column = 0;
+
+    for (size_t i = 0; i < result_length; i++) {
+      result_row = i / result->columns;
+      result_column = i % result->columns;
+
+      input_row = result_column;
+      input_column = result_row;
+
+      result->data[i] = x->data[input_row * x->columns + input_column];
+    }
+  }
+
+  static void parallel_transpose(Matrix *x, Matrix *result) {
+    if (!((x->columns == result->rows) && x->rows == result->columns)) {
+      std::cout << "Error: invalid matrix sizes for transpose.\n";
+    }
+
+    struct process_data_chunk {
+      void operator()(Matrix *x, Matrix *result, int start_index,
+                      int end_index) {
+        int result_row = 0;
+        int result_column = 0;
+
+        int input_row = 0;
+        int input_column = 0;
+
+        for (size_t i = start_index; i < end_index; i++) {
+          result_row = i / result->columns;
+          result_column = i % result->columns;
+
+          input_row = result_column;
+          input_column = result_row;
+
+          result->data[i] = x->data[input_row * x->columns + input_column];
+        }
+      }
+    };
+
+    int result_length = result->rows * result->columns;
+
+    if (!result_length) {
+      return;
+    }
+
+    int min_per_thread = 10000;
+    int max_threads = (result_length + min_per_thread - 1) / min_per_thread;
+    int hardware_threads = std::thread::hardware_concurrency();
+    int num_threads =
+        std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+    int block_size = result_length / num_threads;
+
+    std::vector<std::thread> threads(num_threads);
+    int block_start = 0;
+    int block_end = 0;
+
+    {
+      for (unsigned long i = 0; i < num_threads; i++) {
+        block_start = i * block_size;
+        block_end = block_start + block_size;
+        threads[i] = std::thread(process_data_chunk(), x, result,
+                                 block_start, block_end);
+      }
+
+      for (int i = 0; i < threads.size(); i++) {
+        threads[i].join();
+      }
+
+      process_data_chunk()(result, x, block_end, result_length);
     }
   }
 };
